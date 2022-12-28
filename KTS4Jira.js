ce720a14a0d95cc156c9d7ba868d92e33cfdef99
Boran Gögetap <boran@goegetap.name>
2022-12-28 11:28:25 +0100

    extending with CLI to read issue array from stdin

--- KTS4Jira.js
+++ KTS4Jira.js
@@ -24,3 +24,17 @@ static jiraIssueArray2dotString( issueArray )
 }
 
 }
+
+console.info(chalk.green("reading issue array from stdin. Type CTRL-D to signal end of your input..."));
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// read from stdin
+let stdin = "";
+process.stdin.on('data', function (chunk) {
+    stdin += chunk;
+});
+process.stdin.on('end', function () {
+    const svg = build_diagram_from_string(stdin);
+    return console.log(svg);
+});
