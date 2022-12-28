2ba107a2d09c9624166ced0f595c040e9a92f32c
Boran Gögetap <boran@goegetap.name>
2022-12-28 13:56:10 +0100

    fixing error in label string termination

--- KTS4Jira.js
+++ KTS4Jira.js
@@ -10,7 +10,7 @@ static jiraIssueArray2dotString( issueArray )
 {
     let tempString = "digraph { rankdir=BT ";
     issueArray.forEach(issue => {
-        tempString += "\n<" + issue.key + "> [label=\"" + issue.fields.summary + "\"]";
+        tempString += "\n<" + issue.key + "> [label=\"" + this.safeLabel( issue.fields.summary ) + "\"]";
         const k = issue.key;
         issue.fields.issuelinks.forEach(link => {
             if (link.inwardIssue) {
@@ -23,18 +23,15 @@ static jiraIssueArray2dotString( issueArray )
     return tempString + "\n}";
 }
 
+/*
+ * return a string that is safe to use as a label in a DOT file
+ * by replacing double quotes with escaped double quotes
+ * @param {String} text - text to be used as a label
+ * @returns {String} - safe text to be used as a label which is delimited by double quotes(!)
+ */
+static safeLabel( text )
+{
+    return text.replace( /"/g, "\\\"" )
 }
 
-console.info(chalk.green("reading issue array from stdin. Type CTRL-D to signal end of your input..."));
-
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-// read from stdin
-let stdin = "";
-process.stdin.on('data', function (chunk) {
-    stdin += chunk;
-});
-process.stdin.on('end', function () {
-    const svg = build_diagram_from_string(stdin);
-    return console.log(svg);
-});
+}
