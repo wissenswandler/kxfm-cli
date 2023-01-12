384846b7841ab674d64be090263d760ecf720524
Boran Gögetap <boran@goegetap.name>
2023-01-12 10:37:34 +0100

    check

---
+++ jira-issues2dot.js
@@ -0,0 +1,23 @@
+/*
+ * CLI entry into lightweight KTS lib
+ */
+
+import KTS4Jira from "./KTS4Jira.js";
+
+// read json array from stdin
+let input = "";
+process.stdin.on
+(   "data",
+    (chunk) =>
+    {
+        input += chunk;
+    }
+);
+process.stdin.on
+(   "end",
+    () =>
+    {
+        const issueArray = JSON.parse(input);
+        console.log( KTS4Jira.jiraIssueArray2dotString( issueArray , process.argv[2] )  );
+    }
+);
